2719ea40ff7514ca717365abe58ad196089b7d5d
engrwaseem120 <engrwaseem120@gmail.com>
2025-09-26 18:22:15 +0500

    Added Lecture-14

---
+++ Lecture-14/test1/main.py
@@ -0,0 +1,16 @@
+from agents import Agent, Runner, AsyncOpenAI, set_default_openai_client, set_tracing_disabled, set_default_openai_api
+
+set_tracing_disabled(True)
+set_default_openai_api("chat_completions")
+
+external_client = AsyncOpenAI(
+    api_key= "AIzaSyA8DmLS28hgYGDFl2EoCxgxqm3v9kwvPtM",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+set_default_openai_client(external_client)
+
+agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant", model="gemini-2.0-flash")
+
+result = Runner.run_sync(agent, "Hello")
+
+print(result.final_output)
